065cea86b0ab3fe1aecfc3a3091b3f49bff3b367
Iris <iridian95@live.com>
2023-08-22 13:37:09 -0400

    Adding Youtube Api based on the APOD

---
+++ src/components/Main/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {nasaApi } from "./config";
+import YouTubeSearch from "../Youtube";
+
+
+const Main = () => {
+  const [apodData, setApodData] = useState({});
+  const apiUrl =
+    `https://api.nasa.gov/planetary/apod?api_key=${nasaApi}`;
+    
+  useEffect(() => {
+    // Fetch data from NASA API
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setApodData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h3 id="title" className="pt-5 text-center">
+        {apodData.title}
+      </h3>
+      <p id="date" className="text-center">
+       Date:  {apodData.date}
+      </p>
+      <p id="date" className="lead pt-5">
+        {apodData.explanation}
+      </p>
+      <div id="multimedia" className="text-center">
+        {apodData.media_type === "image" ? (
+          <img src={apodData.url} alt={apodData.title} />
+        ) : (
+          <iframe
+            title={apodData.title}
+            src={apodData.url}
+          />
+        )}
+        <p>Copyright: {apodData.copyright}</p>
+      </div>
+      <YouTubeSearch apodTitle = {apodData.title}/>
+    </div>
+    
+  );
+};
+
+export default Main;
---
+++ src/components/Youtube/index.js
@@ -0,0 +1,57 @@
+import { youtubeApi } from "../Main/config";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Main from "../Main/index"
+
+const YouTubeSearch = ({ apodTitle }) => {
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(() => {
+    if (apodTitle) {
+
+      searchVideos(apodTitle);
+    }
+  }, [apodTitle]);
+
+  const searchVideos = async query => {
+    try {
+      const response = await axios.get(
+        'https://www.googleapis.com/youtube/v3/search',
+        {
+          params: {
+            key: youtubeApi,
+            q: query,
+            part: 'snippet',
+            type: 'video',
+            maxResults: 1,
+          },
+        }
+      );
+      setVideos(response.data.items);
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+    <h2>YouTube Video Related to NASA APOD: {apodTitle}</h2>
+    {videos.length > 0 ? (
+      <div className="video-item">
+        <h3>{videos[0].snippet.title}</h3>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videos[0].id.videoId}`}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+    ) : (
+      <p>No related YouTube video found.</p>
+    )}
+  </div>
+)};
+
+export default YouTubeSearch;
