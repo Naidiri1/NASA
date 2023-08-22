7a62fc1b8e6bbd854ffc59f2825934fff6b1c12c
Iris <iridian95@live.com>
2023-08-22 13:58:58 -0400

    Adding Api to use for Apod

--- src/components/Youtube/index.js
+++ src/components/Youtube/index.js
@@ -1,57 +1,55 @@
 import { youtubeApi } from "../Main/config";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Main from "../Main/index"
 
 const YouTubeSearch = ({ apodTitle }) => {
-  const [videos, setVideos] = useState([]);
-
+  const [video, setVideo] = useState([]);
 
   useEffect(() => {
     if (apodTitle) {
-
       searchVideos(apodTitle);
     }
   }, [apodTitle]);
 
-  const searchVideos = async query => {
+  const searchVideos = async (query) => {
     try {
       const response = await axios.get(
-        'https://www.googleapis.com/youtube/v3/search',
+        "https://www.googleapis.com/youtube/v3/search",
         {
           params: {
             key: youtubeApi,
             q: query,
-            part: 'snippet',
-            type: 'video',
+            part: "snippet",
+            type: "video",
             maxResults: 1,
           },
         }
       );
-      setVideos(response.data.items);
+      setVideo(response.data.items);
     } catch (error) {
-      console.error('Error fetching videos:', error);
+      console.error("Error fetching video:", error);
     }
   };
 
   return (
     <div className="container">
-    <h2>YouTube Video Related to NASA APOD: {apodTitle}</h2>
-    {videos.length > 0 ? (
-      <div className="video-item">
-        <h3>{videos[0].snippet.title}</h3>
-        <iframe
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${videos[0].id.videoId}`}
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
-    ) : (
-      <p>No related YouTube video found.</p>
-    )}
-  </div>
-)};
+      <h2>YouTube Video Related to NASA APOD: {apodTitle}</h2>
+      {video.length > 0 ? (
+        <div className="video-item">
+          <h3>{video[0].snippet.title}</h3>
+          <iframe
+            width="760"
+            height="315"
+            src={`https://www.youtube.com/embed/${video[0].id.videoId}`}
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ) : (
+        <p>No related YouTube video found.</p>
+      )}
+    </div>
+  );
+};
 
 export default YouTubeSearch;
